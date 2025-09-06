b21e515e021a7aa2fdd90533b1d7c5c94e772b5c
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-09-06 11:55:48 +0530

    Initial commit

---
+++ src/main/java/com/raiyyan/tixly/TixlyApplication.java
@@ -0,0 +1,13 @@
+package com.raiyyan.tixly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TixlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TixlyApplication.class, args);
+	}
+
+}
